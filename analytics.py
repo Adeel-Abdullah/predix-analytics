abbb43421c0f2aecdc77f87f690e5b401a1276a8
IC-HO-2032 <syedabdullah94@hotmail.com>
2017-11-29 18:39:07 +0500

    added basic requests

---
+++ analytics.py
@@ -0,0 +1,63 @@
+import requests
+import json
+import base64
+import os
+import shutil
+#%%
+uaaUrl = "https://d25dd486-88f2-4504-b3bf-f38cdc2a64cf.predix-uaa.run.aws-usw02-pr.ice.predix.io/oauth/token"
+client_id="inbox"
+client_secret="Inbox123"
+
+token = str(base64.b64encode(b'inbox:Inbox123'))
+
+
+headers = {
+        'authorization': "Basic " + token,
+        'cache-control': "no-cache",
+        'content-type': "application/x-www-form-urlencoded"
+    }
+response = requests.request('POST', uaaUrl, data="grant_type=client_credentials", headers=headers)
+print(json.loads(response.text))
+token = json.loads(response.text)['access_token']
+
+#%%
+#create a catalog entry
+analytics_catalog="https://predix-analytics-catalog-release.run.aws-usw02-pr.ice.predix.io/api/v1/catalog/analytics"
+analytics_zone_id="78d00131-2c29-43f3-8618-10e675fecf9c"
+headers ={
+        'predix-zone-id': "78d00131-2c29-43f3-8618-10e675fecf9c",
+        'authorization': "Bearer " + token,
+        'content-type': "application/json"
+    }
+body={
+  "name": "demo-adder-py",
+  "version": "v1",
+  "supportedLanguage": "Python",
+  "taxonomyLocation": "",
+  "author": "Adeel",
+  "description": "This analytic does simple math",
+  "customMetadata": "{\"assetid\":\"abc\"}"
+}
+response = requests.request('POST', analytics_catalog, data=json.dumps(body), headers=headers)
+print(json.loads(response.text))
+analytic_id=json.loads(response.text)['id']
+
+#%%
+analytics_uploadurl="https://predix-analytics-catalog-release.run.aws-usw02-pr.ice.predix.io/api/v1/catalog/artifacts"
+headers ={
+        'predix-zone-id': "78d00131-2c29-43f3-8618-10e675fecf9c",
+        'authorization': "Bearer " + token,
+#        'content-type': "multipart/form-data"
+    }
+path="D:\predix\predix-analytics-sample\\analytics\demo-adder"
+dir_name="demo-adder-py"
+output_filename="demo-adder-py"
+shutil.make_archive(os.path.join(path,output_filename), 'zip', os.path.join(path,dir_name))
+body={
+  "file": open(os.path.join(path,output_filename+".zip"), 'rb'),
+  "catalogEntryId": analytic_id,
+  "type": "Executable",
+  "description": "This analytic does simple math"
+  }
+response = requests.request('POST', analytics_uploadurl, data=body, headers=headers)
+print(json.loads(response.text))
