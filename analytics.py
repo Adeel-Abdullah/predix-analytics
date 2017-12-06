6418e1c57bad801b9c41a38237b96b8b4f92d495
IC-HO-2032 <syedabdullah94@hotmail.com>
2017-12-06 11:30:37 +0500

    completed validation of analytics

--- analytics.py
+++ analytics.py
@@ -22,9 +22,9 @@ token = json.loads(response.text)['access_token']
 
 #%%
 #create a catalog entry
-analytics_catalog="https://predix-analytics-catalog-release.run.aws-usw02-pr.ice.predix.io/api/v1/catalog/analytics"
-analytics_zone_id="78d00131-2c29-43f3-8618-10e675fecf9c"
-headers ={
+analytics_catalog = "https://predix-analytics-catalog-release.run.aws-usw02-pr.ice.predix.io/api/v1/catalog/analytics"
+analytics_zone_id = "78d00131-2c29-43f3-8618-10e675fecf9c"
+headers = {
         'predix-zone-id': "78d00131-2c29-43f3-8618-10e675fecf9c",
         'authorization': "Bearer " + token,
         'content-type': "application/json"
@@ -53,11 +53,37 @@ path="D:\predix\predix-analytics-sample\\analytics\demo-adder"
 dir_name="demo-adder-py"
 output_filename="demo-adder-py"
 shutil.make_archive(os.path.join(path,output_filename), 'zip', os.path.join(path,dir_name))
+with open(os.path.join(path,output_filename+".zip"), 'rb') as f:
+    body={
+      "file": f,
+      "catalogEntryId": analytic_id,
+      "type": "Executable",
+      "description": "This analytic does simple math"
+      }
+    response = requests.request('POST', analytics_uploadurl, files=body, headers=headers)
+print(json.loads(response.text))
+
+#%%
+#validate analytic
+analytics_validate="https://predix-analytics-catalog-release.run.aws-usw02-pr.ice.predix.io/api/v1/catalog/analytics/" + analytic_id + "/validation"
+headers ={
+        'predix-zone-id': "78d00131-2c29-43f3-8618-10e675fecf9c",
+        'authorization': "Bearer " + token
+    }
+
 body={
-  "file": open(os.path.join(path,output_filename+".zip"), 'rb'),
-  "catalogEntryId": analytic_id,
-  "type": "Executable",
-  "description": "This analytic does simple math"
-  }
-response = requests.request('POST', analytics_uploadurl, data=body, headers=headers)
+  "number1": 700,
+  "number2": 800
+}
+
+response = requests.request('POST', analytics_validate, data=json.dumps(body), headers=headers)
+print(json.loads(response.text))
+validationRequestId=json.loads(response.text)['validationRequestId']
+#%%
+validation_resulturl="https://predix-analytics-catalog-release.run.aws-usw02-pr.ice.predix.io/api/v1/catalog/analytics/"+analytic_id+"/validation/"+validationRequestId
+headers ={
+        'predix-zone-id': "78d00131-2c29-43f3-8618-10e675fecf9c",
+        'authorization': "Bearer " + token
+    }
+response=requests.request('GET',validation_resulturl,headers=headers)
 print(json.loads(response.text))
